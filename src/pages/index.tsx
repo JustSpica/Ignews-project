bc10ae307fa39057151cbd01123c4806ff4a1f82
Guilherme Spica Luiz <Guilhermespicaluiz@gmail.com>
2022-01-21 03:04:10 -0300

    feature: created stripe integration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
+import { GetServerSideProps } from 'next'
 import Head from 'next/head'
 
 import { SubscribeButton } from '../components'
+import { stripe } from '../services/stripe';
 
 import styles from '../styles/home.module.scss';
 
-export default function Home() {
+type StripeProps = {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: StripeProps) {
   return (
     <>
       <Head>
@@ -18,13 +27,34 @@ export default function Home() {
             the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1KJz2ZErOOJYPOTSeQQen3PV', {
+    expand: ['product']
+  })
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', { 
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  }
+
+  return {
+    props: {
+      price,
+      product, 
+    }
+  }
 }
